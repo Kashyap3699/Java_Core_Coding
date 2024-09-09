8416563af0b9797153ca9bd5bc024c274765385e
Kashyap3699 <kashyap3627@gmail.com>
2024-09-09 12:28:49 +0530

    Update Program_123.java

--- Week_3/Program_123.java
+++ Week_3/Program_123.java
@@ -1,6 +1,32 @@
 package Week_3;
 
+import java.util.Scanner;
+
 public class Program_123 {
 
-    
+
+    // Method to replace all occurrences of a character with another character
+    public static String replaceCharacter(String str, char oldChar, char newChar) {
+        return str.replace(oldChar, newChar);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the string:");
+        String str = scanner.nextLine();
+
+        System.out.println("Please enter the character to be replaced:");
+        char oldChar = scanner.next().charAt(0);
+
+        System.out.println("Please enter the replacement character:");
+        char newChar = scanner.next().charAt(0);
+
+        String result = replaceCharacter(str, oldChar, newChar);
+
+        System.out.println("Resulting String: " + result);
+        scanner.close();
+    }
 }
+
+
+
